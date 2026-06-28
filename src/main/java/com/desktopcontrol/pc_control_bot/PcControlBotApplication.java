f2fa8c4b5d75109d2af73eaadb8d5ff9415bd5cb
dimashyvarski <dima.shyvarski@gmail.com>
2026-06-28 18:58:09 +0300

    Initial commit

---
+++ src/main/java/com/desktopcontrol/pc_control_bot/PcControlBotApplication.java
@@ -0,0 +1,13 @@
+package com.desktopcontrol.pc_control_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcControlBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcControlBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desktopcontrol/pc_control_bot/PcControlBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desktopcontrol.pc_control_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcControlBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
